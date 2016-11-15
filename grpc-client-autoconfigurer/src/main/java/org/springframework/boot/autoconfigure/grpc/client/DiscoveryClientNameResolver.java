2766991fa15cfc5beacc73ee969eff63d2761a9f
Stephan.Maevers <Stephan.Maevers@WSBRE01-SMAE1.intra.brkr.corp>
2016-11-15 11:28:16 +0100

    Updated to gRPC 1.0.1 and Spring Cloud Brixton.SR6

--- grpc-client-autoconfigurer/src/main/java/org/springframework/boot/autoconfigure/grpc/client/DiscoveryClientNameResolver.java
+++ grpc-client-autoconfigurer/src/main/java/org/springframework/boot/autoconfigure/grpc/client/DiscoveryClientNameResolver.java
@@ -25,46 +25,48 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by rayt on 5/17/16.
  */
 public class DiscoveryClientNameResolver extends NameResolver {
-  private final String name;
-  private final DiscoveryClient client;
-  private final Attributes attributes;
-  private Listener listener;
+	private final String name;
+	private final DiscoveryClient client;
+	private final Attributes attributes;
+	private Listener listener;
 
-  public DiscoveryClientNameResolver(String name, DiscoveryClient client, Attributes attributes) {
-    this.name = name;
-    this.client = client;
-    this.attributes = attributes;
-  }
-  @Override
-  public String getServiceAuthority() {
-    return name;
-  }
+	public DiscoveryClientNameResolver(String name, DiscoveryClient client, Attributes attributes) {
+		this.name = name;
+		this.client = client;
+		this.attributes = attributes;
+	}
 
-  @Override
-  public void start(Listener listener) {
-    this.listener = listener;
-    refresh();
-  }
+	@Override
+	public String getServiceAuthority() {
+		return name;
+	}
 
-  @Override
-  public void refresh() {
-    List<ResolvedServerInfo> servers = new ArrayList<ResolvedServerInfo>();
-    for (ServiceInstance serviceInstance : client.getInstances(name)) {
-      System.out.println("Service Instance: " + serviceInstance.getHost() + ":" + serviceInstance.getPort());
-      servers.add(new ResolvedServerInfo(InetSocketAddress.createUnresolved(serviceInstance.getHost(), serviceInstance.getPort()),Attributes.EMPTY));
-    }
-    List<List<ResolvedServerInfo>> list = new ArrayList<>();
-    list.add(servers);
-    this.listener.onUpdate(list, Attributes.EMPTY);
-  }
+	@Override
+	public void start(Listener listener) {
+		this.listener = listener;
+		refresh();
+	}
 
-  @Override
-  public void shutdown() {
-  }
+	@Override
+	public void refresh() {
+		List<ResolvedServerInfo> servers = new ArrayList<ResolvedServerInfo>();
+		for (ServiceInstance serviceInstance : client.getInstances(name)) {
+			System.out.println("Service Instance: " + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+			servers.add(new ResolvedServerInfo(
+					InetSocketAddress.createUnresolved(serviceInstance.getHost(), serviceInstance.getPort()),
+					Attributes.EMPTY));
+		}
+		this.listener.onUpdate(Collections.singletonList(servers), Attributes.EMPTY);
+	}
+
+	@Override
+	public void shutdown() {
+	}
 }
